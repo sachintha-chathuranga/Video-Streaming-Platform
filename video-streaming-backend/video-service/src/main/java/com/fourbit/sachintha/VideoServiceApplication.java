a98fe75363a4feeb7e32818944c99a34975f5185
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-26 22:23:42 +0530

    Create Video model

--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/VideoServiceApplication.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/VideoServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideoStreamingPlatformApplication {
+public class VideoServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(VideoStreamingPlatformApplication.class, args);
+    SpringApplication.run(VideoServiceApplication.class, args);
   }
 
 }
---
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/VideoStatus.java
@@ -0,0 +1,5 @@
+package com.fourbit.sachintha.model;
+
+public class VideoStatus {
+
+}
