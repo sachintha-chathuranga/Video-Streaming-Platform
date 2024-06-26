4cc4defc568d79bcce3077302091e8292af1f344
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-27 00:36:47 +0530

    add data.sql in to resources folder

--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/Comment.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/Comment.java
@@ -1,17 +1,19 @@
 package com.fourbit.sachintha.model;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Entity
+@Table(name = "comments")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Comment {
