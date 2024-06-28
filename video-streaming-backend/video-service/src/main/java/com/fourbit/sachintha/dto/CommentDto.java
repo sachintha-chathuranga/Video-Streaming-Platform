c7fbd4e0bb1fd368bde19f68d7ccabe7781f9985
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-28 18:49:37 +0530

    create Comment Dto class

---
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/dto/CommentDto.java
@@ -0,0 +1,17 @@
+package com.fourbit.sachintha.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
+  private Long id;
+  private String text;
+  private VideoDto video;
+  private UserDto user;
+  private Integer likeCount;
+  private Integer dislikeCount;
+}
