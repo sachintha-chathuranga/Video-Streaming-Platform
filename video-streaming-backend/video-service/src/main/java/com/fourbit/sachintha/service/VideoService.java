a92d369573e57dcdcf7bda1c5f8862cd626df3a6
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-28 18:52:55 +0530

    create new video service for add video details

--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/service/VideoService.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/service/VideoService.java
@@ -3,6 +3,8 @@ package com.fourbit.sachintha.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fourbit.sachintha.dto.VideoDto;
+import com.fourbit.sachintha.mapper.VideoMapper;
 import com.fourbit.sachintha.model.Video;
 import com.fourbit.sachintha.repository.VideoRepository;
 
@@ -20,5 +22,27 @@ public class VideoService {
     video.setVideoUrl(videoUrl);
     videoRepository.save(video);
     return videoUrl;
+  }
+
+  public VideoDto updateVideoMetaData(VideoDto videoDto) {
+    Video video = videoRepository.findById(videoDto.getId())
+        .orElseThrow(() -> new IllegalArgumentException("Video does not exists!"));
+    if (videoDto.getDescription() != null) {
+      video.setDescription(videoDto.getDescription());
+    }
+    if (videoDto.getTitle() != null) {
+      video.setTitle(videoDto.getTitle());
+    }
+    if (videoDto.getUserId() != null) {
+      video.setUserId(videoDto.getUserId());
+    }
+    if (videoDto.getTags() != null) {
+      video.setTags(videoDto.getTags());
+    }
+    if (videoDto.getVideoStatus() != null) {
+      video.setVideoStatus(videoDto.getVideoStatus());
+    }
+    Video savedVideo = videoRepository.save(video);
+    return VideoMapper.mapToVideoDto(savedVideo);
   }
 }
