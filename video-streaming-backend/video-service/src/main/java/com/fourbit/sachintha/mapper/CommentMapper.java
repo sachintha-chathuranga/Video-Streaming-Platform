b3d029387b9f072443ca6dbd2aad33419f55e718
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-30 19:32:53 +0530

    Update all mappers

--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/mapper/CommentMapper.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/mapper/CommentMapper.java
@@ -5,10 +5,12 @@ import com.fourbit.sachintha.model.Comment;
 
 public class CommentMapper {
   public static Comment mapToComment(CommentDto commentDto) {
+    if (commentDto == null) {
+      return null;
+    }
     Comment comment = new Comment(
         commentDto.getId(),
         commentDto.getText(),
-        VideoMapper.mapToVideo(commentDto.getVideo()),
         UserMapper.mapToUser(commentDto.getUser()),
         commentDto.getLikeCount(),
         commentDto.getDislikeCount());
@@ -16,9 +18,11 @@ public class CommentMapper {
   }
 
   public static CommentDto mapToCommentDto(Comment comment) {
+    if (comment == null) {
+      return null;
+    }
     CommentDto commentDto = new CommentDto(comment.getId(),
         comment.getText(),
-        VideoMapper.mapToVideoDto(comment.getVideo()),
         UserMapper.mapToUserDto(comment.getUser()),
         comment.getLikeCount(),
         comment.getDislikeCount());
--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/mapper/UserMapper.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/mapper/UserMapper.java
@@ -12,69 +12,29 @@ import com.fourbit.sachintha.model.User;
 import com.fourbit.sachintha.model.Video;
 
 public class UserMapper {
-    public static User mapToUser(UserDto userDto) {
-        List<Comment> comments = Optional.ofNullable(userDto.getComments()).orElse(Collections.emptyList()).stream()
-                .map(commentObj -> CommentMapper.mapToComment(commentObj)).toList();
-        List<User> subscriptions = Optional.ofNullable(userDto.getSubscriptions()).orElse(Collections.emptyList())
-                .stream()
-                .map(userObj -> mapToUser(userObj)).toList();
-        List<User> subscribers = Optional.ofNullable(userDto.getSubscribers()).orElse(Collections.emptyList()).stream()
-                .map(userObj -> mapToUser(userObj)).toList();
-        List<Video> videoHistory = Optional.ofNullable(userDto.getVideoHistory()).orElse(Collections.emptyList())
-                .stream()
-                .map(videoObj -> VideoMapper.mapToVideo(videoObj))
-                .toList();
-        List<Video> likedVideos = Optional.ofNullable(userDto.getLikedVideos()).orElse(Collections.emptyList()).stream()
-                .map(videoObj -> VideoMapper.mapToVideo(videoObj))
-                .toList();
-        List<Video> dislikedVideos = Optional.ofNullable(userDto.getDislikedVideos()).orElse(Collections.emptyList())
-                .stream().map(videoObj -> VideoMapper.mapToVideo(videoObj))
-                .toList();
-        User user = new User(
-                userDto.getId(),
-                userDto.getFirstName(),
-                userDto.getLastName(),
-                userDto.getPictureUrl(),
-                userDto.getAbout(),
-                comments,
-                subscriptions,
-                subscribers,
-                videoHistory,
-                likedVideos,
-                dislikedVideos);
-        return user;
-    }
 
-    public static UserDto mapToUserDto(User user) {
-        List<CommentDto> comments = Optional.ofNullable(user.getComments()).orElse(Collections.emptyList()).stream()
-                .map(commentObj -> CommentMapper.mapToCommentDto(commentObj)).toList();
-        List<UserDto> subscriptions = Optional.ofNullable(user.getSubscriptions()).orElse(Collections.emptyList())
-                .stream()
-                .map(userObj -> mapToUserDto(userObj)).toList();
-        List<UserDto> subscribers = Optional.ofNullable(user.getSubscribers()).orElse(Collections.emptyList()).stream()
-                .map(userObj -> mapToUserDto(userObj)).toList();
-        List<VideoDto> videoHistory = Optional.ofNullable(user.getVideoHistory()).orElse(Collections.emptyList())
-                .stream()
-                .map(videoObj -> VideoMapper.mapToVideoDto(videoObj))
-                .toList();
-        List<VideoDto> likedVideos = Optional.ofNullable(user.getLikedVideos()).orElse(Collections.emptyList()).stream()
-                .map(videoObj -> VideoMapper.mapToVideoDto(videoObj))
-                .toList();
-        List<VideoDto> dislikedVideos = Optional.ofNullable(user.getDislikedVideos()).orElse(Collections.emptyList())
-                .stream()
-                .map(videoObj -> VideoMapper.mapToVideoDto(videoObj))
-                .toList();
-        UserDto userDto = new UserDto(user.getId(),
-                user.getFirstName(),
-                user.getLastName(),
-                user.getPictureUrl(),
-                user.getAbout(),
-                comments,
-                subscriptions,
-                subscribers,
-                videoHistory,
-                likedVideos,
-                dislikedVideos);
-        return userDto;
-    }
+        public static User mapToUser(UserDto userDto) {
+                if (userDto == null) {
+                        return null;
+                }
+                User user = new User(
+                                userDto.getId(),
+                                userDto.getFirstName(),
+                                userDto.getLastName(),
+                                userDto.getPictureUrl(),
+                                userDto.getAbout());
+                return user;
+        }
+
+        public static UserDto mapToUserDto(User user) {
+                if (user == null) {
+                        return null;
+                }
+                UserDto userDto = new UserDto(user.getId(),
+                                user.getFirstName(),
+                                user.getLastName(),
+                                user.getPictureUrl(),
+                                user.getAbout());
+                return userDto;
+        }
 }
