8fd40baed60d3360b65cf8a0d5774de73a812dc6
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-30 10:27:20 +0530

    add OAuth2 security for api

---
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.fourbit.sachintha.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+  private String issuer;
+
+  @Value("${auth0.audience}")
+  private String audience;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((authorize) -> authorize
+            .anyRequest().authenticated())
+        .oauth2ResourceServer((oauth2) -> oauth2
+            .jwt(Customizer.withDefaults())).sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+    return http.build();
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(issuer);
+
+    // OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+    // OAuth2TokenValidator<Jwt> withIssure = JwtValidators.createDefaultWithIssuer(issuer);
+    // OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssure, audienceValidator);
+
+    // jwtDecoder.setJwtValidator(withAudience);
+    return jwtDecoder;
+  }
+}
