b6713aab9dab90196371adef2f751efd52d51a1d
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-06-30 19:32:30 +0530

    Update models

--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/Video.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/Video.java
@@ -1,12 +1,17 @@
 package com.fourbit.sachintha.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,21 +29,46 @@ public class Video {
   private Long id;
   private String description;
   private String title;
-  private Long userId;
 
-  @ManyToMany(mappedBy = "likedVideos")
-  private List<User> likes;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "userId")
+  private User user;
 
-  @ManyToMany(mappedBy = "dislikedVideos")
-  private List<User> dislikes;
-  private List<String> tags;
   private String videoUrl;
   private VideoStatus videoStatus;
-
-  @ManyToMany(mappedBy = "videoHistory")
-  private List<User> views;
   private String thumbnailUrl;
 
-  @OneToMany(mappedBy = "video")
-  private List<Comment> comments;
+  private List<String> tags = new ArrayList<>();
+
+  @ManyToMany(mappedBy = "likedVideos")
+  private List<User> likes = new ArrayList<>();
+
+  @ManyToMany(mappedBy = "dislikedVideos")
+  private List<User> dislikes = new ArrayList<>();
+
+  @ManyToMany(mappedBy = "videoHistory")
+  private List<User> views = new ArrayList<>();
+
+  @OneToMany(mappedBy = "video", cascade = CascadeType.ALL)
+  private List<Comment> comments = new ArrayList<>();
+
+  public Video(
+      Long id,
+      String description,
+      String title,
+      User user,
+      String videoUrl,
+      VideoStatus videoStatus,
+      String thumbnailUrl,
+      List<String> tags) {
+    this.id = id;
+    this.description = description;
+    this.title = title;
+    this.user = user;
+    this.videoUrl = videoUrl;
+    this.videoStatus = videoStatus;
+    this.thumbnailUrl = thumbnailUrl;
+    this.tags = tags;
+  }
+
 }
