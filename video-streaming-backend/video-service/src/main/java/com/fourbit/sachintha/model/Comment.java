76d10608ace262d62b0fad211bf7c7d8469f199a
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-01 10:10:23 +0530

    Create add like for comment route

--- video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/Comment.java
+++ video-streaming-backend/video-service/src/main/java/com/fourbit/sachintha/model/Comment.java
@@ -2,6 +2,7 @@ package com.fourbit.sachintha.model;
 
 import com.fourbit.sachintha.dto.UserDto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -23,6 +24,7 @@ public class Comment {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+  @Column(nullable = false)
   private String text;
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -32,7 +34,11 @@ public class Comment {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "userId")
   private User user;
+
+  @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
   private Integer likeCount;
+
+  @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
   private Integer dislikeCount;
 
   public Comment(
@@ -46,5 +52,39 @@ public class Comment {
     this.user = user;
     this.likeCount = likeCount;
     this.dislikeCount = dislikeCount;
+  }
+
+  public void incrementLikeCount() {
+    if (this.likeCount == null) {
+      this.likeCount = 0;
+    }
+    this.likeCount += 1;
+  }
+
+  public void decrementLikeCount() {
+    if (this.likeCount == null) {
+      this.likeCount = 0;
+    }
+    if (this.likeCount<=0) {
+      return;
+    }
+    this.likeCount -= 1;
+  }
+
+  public void incrementDislikeCount() {
+    if (this.dislikeCount == null) {
+      this.dislikeCount = 0;
+    }
+    this.dislikeCount += 1;
+  }
+
+  public void decrementDislikeCount() {
+    if (this.dislikeCount == null) {
+      this.dislikeCount = 0;
+    }
+    if (this.dislikeCount <= 0) {
+      return;
+    }
+    this.dislikeCount -= 1;
   }
 }
