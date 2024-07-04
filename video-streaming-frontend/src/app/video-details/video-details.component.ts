bc0cf9bd29121a5329127171a0e009f9c0860a2a
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-04 16:14:44 +0530

    Create video details component

---
+++ video-streaming-frontend/src/app/video-details/video-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-details.component.html',
+  styleUrl: './video-details.component.css'
+})
+export class VideoDetailsComponent {
+
+}
