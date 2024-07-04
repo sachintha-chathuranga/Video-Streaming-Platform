b71c27145fb3cd3d8ba9b192778f53758deaeb95
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-04 16:13:09 +0530

    modify HttpClient request with adding auth token using interceptor

--- video-streaming-frontend/src/app/auth/auth.config.ts
+++ video-streaming-frontend/src/app/auth/auth.config.ts
@@ -2,12 +2,18 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'http://localhost:8080/realms/local-keycloak',
+    authority: 'http://localhost:8181/realms/local-keycloak',
     redirectUrl: 'http://localhost:4200',
-    clientId: 'local-app',
-    scope: 'openid profile offline_access',
+    clientId: 'app-client',
+    scope: 'openid profile offline_access email',
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
+    secureRoutes: [
+      'http://localhost:8080/'
+    ],
+    customParamsAuthRequest: {
+      audience: "http://localhost:8080/"
+    }
   },
 };
