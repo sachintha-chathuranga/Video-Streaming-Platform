138017250362f57182318ce891d26071a9bb74c9
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-06 19:54:51 +0530

    update routes and video details component

--- video-streaming-frontend/src/app/app.routes.ts
+++ video-streaming-frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { VideoUploadComponent } from './video-upload/video-upload.component';
 import { VideoFormComponent } from './video-form/video-form.component';
 import { VideoDetailsComponent } from './video-details/video-details.component';
+import { HomeComponent } from './home/home.component';
+import { CallbackComponent } from './callback/callback.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: VideoUploadComponent,
+    component: HomeComponent,
   },
   {
     path: 'upload-video',
@@ -20,4 +22,8 @@ export const routes: Routes = [
     path: 'video-details/:videoId',
     component: VideoDetailsComponent,
   },
+  {
+    path: 'callback',
+    component: CallbackComponent
+  }
 ];
--- video-streaming-frontend/src/app/video-details/video-details.component.ts
+++ video-streaming-frontend/src/app/video-details/video-details.component.ts
@@ -3,13 +3,15 @@ import { ActivatedRoute } from '@angular/router';
 import { VideoService } from '../video.service';
 import { VideoPlayerComponent } from '../video-player/video-player.component';
 import { VideoDto } from '../dto/video.dto';
+import { UserService } from '../user.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-video-details',
   standalone: true,
-  imports: [VideoPlayerComponent],
+  imports: [VideoPlayerComponent, MatSnackBarModule],
   templateUrl: './video-details.component.html',
-  styleUrl: './video-details.component.css'
+  styleUrl: './video-details.component.css',
 })
 export class VideoDetailsComponent {
   videoId!: string;
@@ -19,19 +21,27 @@ export class VideoDetailsComponent {
   tags: Array<string> = [];
   videoAvailability: boolean = false;
 
-
-  constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService) {
-    this.videoId = this.activatedRoute.snapshot.params["videoId"];
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private videoService: VideoService,
+    private userService: UserService,
+    private snackBar: MatSnackBar
+  ) {
+    this.videoId = this.activatedRoute.snapshot.params['videoId'];
     this.videoService.getVideoById(this.videoId).subscribe((data: VideoDto) => {
       this.videoUrl = data.videoUrl;
       this.videoTitle = data.title;
       this.videoDescription = data.description;
       this.tags = data.tags;
       this.videoAvailability = true;
-    })
+    });
   }
 
-  ngOnInit(): void {
-
+  subscribe() {
+    this.userService
+      .subscribeChannel(this.videoId)
+      .subscribe((data) => {
+        this.snackBar.open(`${data}`, 'OK');
+      }, (error) => this.snackBar.open(`${error}`, "OK"));
   }
 }
