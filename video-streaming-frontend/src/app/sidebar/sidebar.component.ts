a7e9bfe1d81896dd4822cfb3baddd894a243d685
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-06 19:54:19 +0530

    create sidebar component

---
+++ video-streaming-frontend/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatSidenavModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
