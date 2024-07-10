185a4caf2505241a0eb0227e2c736e2c727aed84
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-10 13:46:54 +0530

    update video-upload component

--- video-streaming-frontend/src/app/components/video-upload/UploadVideoResponse.ts
+++
@@ -1,4 +0,0 @@
-export interface UploadVideoResponse {
-  videoId: number;
-  videoUrl: string;
-}
--- video-streaming-frontend/src/app/components/video-upload/video-upload.component.ts
+++ video-streaming-frontend/src/app/components/video-upload/video-upload.component.ts
@@ -9,18 +9,55 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { VideoService } from '../../services/video.service';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 @Component({
   selector: 'app-video-upload',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatButtonModule, NgxFileDropModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgxFileDropModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './video-upload.component.html',
   styleUrl: './video-upload.component.css',
+  animations: [
+    trigger('uploadAnimation', [
+      state(
+        'start',
+        style({
+          width: '0%',
+        })
+      ),
+      state(
+        'end',
+        style({
+          width: '100%',
+        })
+      ),
+      transition('start => end', [animate('2s')]),
+    ]),
+  ],
 })
 export class VideoUploadComponent {
   public files: NgxFileDropEntry[] = [];
   isFileSelect: boolean = true;
   fileEntry: FileSystemFileEntry | undefined;
+  // animationState = 'start';
+  isLoading = false;
+  uploadProgress = 0;
 
   constructor(private videoService: VideoService, private router: Router) {}
 
@@ -51,14 +88,25 @@ export class VideoUploadComponent {
   }
 
   public uploadVideo() {
-    if (this.fileEntry) {
-      console.log('Start File upload...');
-      // this.fileEntry.file((file: File) => {
-      //   this.videoService.uploadVideo(file).subscribe((data) => {
-      //     this.router.navigateByUrl("/update-video-details/" + data.videoId);
-      //   });
-      // });
-    }
-    this.router.navigateByUrl('/update-video-details/' + 102);
+    // this.animationState = 'end';
+    // if (this.fileEntry) {
+    //   console.log('Start File upload...');
+    // this.fileEntry.file((file: File) => {
+    //   this.videoService.uploadVideo(file).subscribe((data) => {
+    //     this.router.navigateByUrl("/update-video-details/" + data.videoId);
+    //   });
+    // });
+    // }
+    // this.router.navigateByUrl('/update-video-details/' + 102);
+    this.isLoading = true;
+    this.uploadProgress = 0;
+    const interval = setInterval(() => {
+      if (this.uploadProgress < 100) {
+        this.uploadProgress += 10;
+      } else {
+        clearInterval(interval);
+        this.isLoading = false;
+      }
+    }, 200);
   }
 }
