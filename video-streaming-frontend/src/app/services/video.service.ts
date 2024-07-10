b0086f89c85e855c1886d0f697abb36be6ca9a57
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-10 13:47:16 +0530

    add new video service

--- video-streaming-frontend/src/app/services/video.service.ts
+++ video-streaming-frontend/src/app/services/video.service.ts
@@ -2,7 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { VideoDto } from '../dto/video.dto';
-import { UploadVideoResponse } from '../components/video-upload/UploadVideoResponse';
+import { UploadVideoResponse } from '../dto/uploadResponse.dto';
+
 
 @Injectable({
   providedIn: 'root',
