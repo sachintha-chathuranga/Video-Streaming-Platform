5ad6ef303253a8123c92d88770223bdce9d2e03d
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-07-10 13:48:26 +0530

    initialize personal-data component

---
+++ video-streaming-frontend/src/app/pages/personal-data/personal-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-data.component.html',
+  styleUrl: './personal-data.component.css'
+})
+export class PersonalDataComponent {
+
+}
