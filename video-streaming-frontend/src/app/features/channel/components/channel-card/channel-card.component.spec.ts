5d358fc889f5baf23913b330a0122106b8ab7065
sachintha-chathuranga <mmschathuranga1@gmail.com>
2024-12-03 00:46:09 +0530

    save

--- video-streaming-frontend/src/app/features/channel/components/channel-card/channel-card.component.spec.ts
+++ video-streaming-frontend/src/app/features/channel/components/channel-card/channel-card.component.spec.ts
@@ -1,23 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ChannelCardComponent } from './channel-card.component';
 
 describe('ChannelCardComponent', () => {
-  let component: ChannelCardComponent;
-  let fixture: ComponentFixture<ChannelCardComponent>;
+	let component: ChannelCardComponent;
+	let fixture: ComponentFixture<ChannelCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ChannelCardComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ChannelCardComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(ChannelCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(ChannelCardComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
